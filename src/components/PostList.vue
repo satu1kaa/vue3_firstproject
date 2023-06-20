8b4885269090fefe5e936d314ebe9ca83686a8fc
satu1kaa <satu1kaa@gmail.com>
2023-06-20 12:15:54 +0700

    Add several elements

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,11 +1,18 @@
 <template >
-    <div>
+    <div v-show="posts.length > 0">
         <h3>Список постов</h3>
-        <post-item 
-            v-for="post in posts"
-            :post="post"
-        />
+        <transition-group name="post-list">
+            <post-item 
+                v-for="post in posts"
+                :post="post"
+                :key="post.id"
+                @remove="$emit('remove', post)"
+            />
+        </transition-group>
     </div>
+    <h2 v-show="posts.length === 0" style="color: red">
+        Список постов пуст
+    </h2>
 </template>
 
 <script>
@@ -23,5 +30,23 @@ export default {
 </script>
 
 <style scoped>
-
+.post-list-item {
+    display: inline-block;
+    margin-right: 10px;
+}
+.post-list-enter-active,
+.post-list-leave-active {
+    transition: all 0.4s ease;
+}
+.post-list-enter-from {
+    opacity: 0;
+    transform: translateX(-50px);
+}
+.post-list-leave-to {
+    opacity: 0;
+    transform: translateX(50px);
+}
+.post-list-move {
+    transition: transform 0.4s ease;
+}
 </style>
